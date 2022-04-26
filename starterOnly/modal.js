ddbfdad2f8ed2f38cdb93c253d16da5b34af47b7
EPourmo <94918200+EPourmo@users.noreply.github.com>
2022-04-26 15:55:34 +0200

    first commit with changes : issues 1 & 2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,8 +9,11 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalClose = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("form");
+// let errorValue = true;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +23,17 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal event
+modalClose.addEventListener("click", closeModal);
 
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// form prevent default if error
+// form.addEventListener("submit", (e) => {
+//   if (errorValue) {
+//     e.preventDefault();
+//   }
+// });
